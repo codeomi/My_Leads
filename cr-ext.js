141f86d8480a81a4956e143fa55c809e0c789327
Omkar Kudale <omkarkudale007@gmail.com>
2022-09-04 23:39:24 +0530

    adding files

---
+++ cr-ext.js
@@ -0,0 +1,50 @@
+let myLeads = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("text-el")
+const ulEl = document.getElementById("ul-l")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+deleteBtn.addEventListener("dblclick", function () {
+       myLeads = []
+       localStorage.clear()
+       render(myLeads)
+
+})
+
+if (leadsFromLocalStorage) {
+       myLeads = leadsFromLocalStorage
+       render(myLeads)
+}
+
+tabBtn.addEventListener("click", function () {
+       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+              myLeads.push(tabs[0].url)
+              localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+              render(myLeads)
+       })
+})
+
+function render(leads) {
+       let listItems = ""
+       for (let i = 0; i < leads.length; i++) {
+
+              listItems += `
+              <li>
+                     <a target = '_blank' href =  ${leads[i]} > ${leads[i]} 
+                     </a>
+              </li>`
+       }
+
+       ulEl.innerHTML = listItems
+}
+
+inputBtn.addEventListener("click", function () {
+       myLeads.push(inputEl.value)
+       inputEl.value = ""
+       localStorage.setItem("myLeads", JSON.stringify(myLeads))
+       render(myLeads)
+})
+
+
